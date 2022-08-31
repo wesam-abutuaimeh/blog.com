8d00dff48e21e30401f6d3f4e6fc98c005be21e8
Wesam Abutaima <wesam.abutuaimeh@gmail.com>
2022-08-31 13:04:18 +0300

    animation

--- main.js
+++ main.js
@@ -1,11 +1,69 @@
 "use-strict"; /* strict mode */
 
+/* Page Loading popup */
+const loader = document.querySelector(".loader");
+document.onreadystatechange = () => {
+  switch (document.readyState) {
+    case "complete":
+      loader.classList.add("remove");
+      let welcomePhrase = document.createElement("p");
+      welcomePhrase.className = "welcome-phrase";
+      welcomePhrase.style.cssText =
+        "position: absolute; top: 0; left: 50%; transform: translateX(-50%); display: flex; justify-content: center; align-items: center; width: 100%; height: 100%; background-image: var(--main-btn-gradient); color: #FFFFFF; font-weight: 600; letter-spacing: 3px; transition:0.3s ease-in-out";
+      welcomePhrase.appendChild(
+        document.createTextNode(
+          `Welcome, I wish you a nice trip in my beautiful site`
+        )
+      );
+      const welcomePhraseAppend = document.body.appendChild(welcomePhrase);
+      setTimeout(() => {
+        welcomePhraseAppend.style.display = "none";
+      }, 2000);
+      break;
+    case "interactive":
+      loader.classList.add("show");
+      break;
+    case "loading":
+      loader.classList.add("show");
+      break;
+  }
+};
+
 /* Welcome phrase to the user who opens the browser console */
 console.log(
   "%cWelcom To Wesam%cBlog",
   "color:red; font-size:40px",
   "background-color:blue;color:white; font-size:40px"
 );
+
+/* colors option from setting box */
+document.querySelector(".icon").onclick = (_) => {
+  const settingBox = document.querySelector(".setting-box");
+  settingBox.classList.toggle("show");
+};
+
+if (localStorage.getItem("colors")) {
+  document.documentElement.style.setProperty(
+    "--main-color-",
+    localStorage.getItem("colors")
+  );
+  console.log(
+    `The Hex Code For The Color I present Now Is \`${localStorage.getItem(
+      "colors"
+    )}\``
+  );
+}
+
+let colorsList = document.querySelectorAll(".colors-options li");
+colorsList.forEach((li) => {
+  li.addEventListener("click", (event) => {
+    document.documentElement.style.setProperty(
+      "--main-color-",
+      event.target.dataset.color
+    );
+    localStorage.setItem("colors", event.target.dataset.color);
+  });
+});
 
 /* Subscribe Form Validation */
 let subscribeVaild = document.querySelector("#email");
@@ -71,60 +129,15 @@ scroolBtn.onclick = function () {
   });
 };
 
-/* colors option from setting box */
-document.querySelector(".icon").onclick = (_) => {
-  const settingBox = document.querySelector(".setting-box");
-  settingBox.classList.toggle("show");
-};
+// animation on skill section on scroll
+let section = document.querySelector(".skills");
+let spans = document.querySelectorAll(".progress span");
 
-if (localStorage.getItem("colors")) {
-  document.documentElement.style.setProperty(
-    "--main-color-",
-    localStorage.getItem("colors")
-  );
-  console.log(
-    `The Hex Code For The Color I present Now Is \`${localStorage.getItem(
-      "colors"
-    )}\``
-  );
-}
-
-let colorsList = document.querySelectorAll(".colors-options li");
-colorsList.forEach((li) => {
-  li.addEventListener("click", (event) => {
-    document.documentElement.style.setProperty(
-      "--main-color-",
-      event.target.dataset.color
-    );
-    localStorage.setItem("colors", event.target.dataset.color);
-  });
-});
-
-/* Page Loading popup */
-const loader = document.querySelector(".loader");
-document.onreadystatechange = () => {
-  switch (document.readyState) {
-    case "complete":
-      loader.classList.add("remove");
-      let welcomePhrase = document.createElement("p");
-      welcomePhrase.className = "welcome-phrase";
-      welcomePhrase.style.cssText =
-        "position: absolute; top: 0; left: 50%; transform: translateX(-50%); display: flex; justify-content: center; align-items: center; width: 100%; height: 100%; background-image: var(--main-btn-gradient); color: #FFFFFF; font-weight: 600; letter-spacing: 3px; transition:0.3s ease-in-out";
-      welcomePhrase.appendChild(
-        document.createTextNode(
-          `Welcome, I wish you a nice trip in my beautiful site`
-        )
-      );
-      const welcomePhraseAppend = document.body.appendChild(welcomePhrase);
-      setTimeout(() => {
-        welcomePhraseAppend.style.display = "none";
-      }, 2000);
-      break;
-    case "interactive":
-      loader.classList.add("show");
-      break;
-    case "loading":
-      loader.classList.add("show");
-      break;
+window.onscroll = function () {
+  if (window.scrollY >= section.offsetTop) {
+    console.log("Reached Section Three");
+    spans.forEach((span) => {
+      span.style.width = span.dataset.width;
+    });
   }
 };
