e478b31c8172c29c3b99daf4832deeef4b8ae330
Wesam-abutuaimeh <wesam-abutuaimeh@gmail.com>
2022-10-15 19:46:49 +0300

    update:

---
+++ js/module.js
@@ -0,0 +1,23 @@
+"use-strict";
+
+// Function For Traking Events
+export function foEvents(startTrakingDate) {
+  const targetDate = new Date(startTrakingDate).getTime();
+  const interval = setInterval(() => {
+    const currentDate = new Date().getTime(); // Start Date
+    const elapsed = targetDate - currentDate;
+    const years = Math.trunc(elapsed / (1000 * 60 * 60 * 24 * 365));
+    const days = Math.trunc(elapsed / (1000 * 60 * 60 * 24));
+    const hours = Math.trunc(
+      (elapsed % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.trunc((elapsed % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.trunc((elapsed % (1000 * 60)) / 1000);
+    document.querySelector(".years").innerHTML = years;
+    document.querySelector(".days").innerHTML = days;
+    document.querySelector(".hours").innerHTML = hours;
+    document.querySelector(".minutes").innerHTML = minutes;
+    document.querySelector(".seconds").innerHTML = seconds;
+    elapsed < 0 ? clearInterval(interval) : "";
+  }, 1000);
+}
